d73438b3582f5c69fd2cc1bb1c73e62a61d28c0c
Aurélien Pouilles <aurelien.pouilles.pro@hotmail.com>
2025-10-08 16:06:57 +0000

    TP2 q4

--- tp2/tp.c
+++ tp2/tp.c
@@ -1,17 +1,27 @@
 /* GPLv2 (c) Airbus */
 #include <debug.h>
+#include "intr.h"
 
 void bp_handler() {
-   // TODO
+	//QUESTION 2
+   debug("Handling interruption\n");
+   asm volatile("leave\niret");
 }
 
 void bp_trigger() {
-	// TODO
+	//QUESTION 4
+	asm volatile("int3"::);
 }
 
 void tp() {
-	// TODO print idtr
+	// QUESTION 1
+	idt_reg_t idtr_reg;
+	get_idtr(idtr_reg);
+	debug("IDTR address: %lx\n", idtr_reg.addr);
 
-	// TODO call bp_trigger
-   //bp_trigger();
+	//QUESTION 3
+	idtr_reg.desc[3].offset_1 = ((uint32_t)&bp_handler) & 0xffff;
+	idtr_reg.desc[3].offset_2 = ((uint32_t)&bp_handler >> 16) & 0xffff;
+
+	bp_trigger();
 }
