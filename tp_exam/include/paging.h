861ca84c512ba91130962b4a813ebd0be8480296
kmarty <killianmarty@outlook.com>
2025-11-27 17:40:23 +0100

    All in one file

--- tp_exam/include/paging.h
+++ tp_exam/include/paging.h
@@ -1,13 +1,18 @@
 #ifndef PAGING_H
 #define PAGING_H
 
-#include <pagemem.h>
-#include "utils.h"
+#include <types.h>
 
-extern pde32_t *pgd;
+// Définition des adresses partagées (si pas déjà fait ailleurs)
+#define SHARED_ADDR_U1  0xCAFE0000
+#define SHARED_ADDR_U2  0xBEEF0000
 
-void init_pagination(void);
-void activate_pagination(void);
-void map_page(uint32_t phys_addr, uint32_t virt_addr, uint32_t flags);
+// Prototypes
+void init_pagination();
+void activate_pagination();
+
+// Fonctions pour récupérer les PGD des tâches (pour le main.c)
+uint32_t get_pgd_t1_phys();
+uint32_t get_pgd_t2_phys();
 
 #endif
